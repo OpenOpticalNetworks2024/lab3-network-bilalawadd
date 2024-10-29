1c1a316086f51212491391353d3b51f673d6c7b2
bilal awad <s320760@studenti.polito.it>
2024-10-29 18:41:03 +0100

    Lab3_update1.2

--- core/elements.py
+++ core/elements.py
@@ -2,92 +2,109 @@ import json
 
 class Signal_information(object):
     def __init__(self, signal_power: float, path: list[str]):
-        self.signal_power = signal_power
-        self.noise_power = 0.0
-        self.latency = 0.0
-        self.path = path
+        self._signal_power = signal_power
+        self._noise_power = 0.0
+        self._latency = 0.0
+        self._path = path
 
 
     @property
     def signal_power(self):
-       return self.signal_power
+       return self._signal_power
 
 
 
     def update_signal_power(self,delta_signal_power: float):
-       self.signal_power +=delta_signal_power
+       self._signal_power +=delta_signal_power
 
 
     @property
     def noise_power(self):
-        return self.noise_power
+        return self._noise_power
 
 
     @noise_power.setter
     def noise_power(self,noise: float):
-         self.noise_power = noise
+         self._noise_power = noise
 
 
     def update_noise_power(self,delt_noise: float):
-        self.noise_power += delt_noise
+        self._noise_power += delt_noise
 
 
     @property
     def latency(self):
-        return self.latency
+        return self._latency
 
 
     @latency.setter
     def latency(self,latency: float):
-        self.latency = latency
+        self._latency = latency
 
 
     def update_latency(self,delta_latency: float):
-        self.latency += delta_latency
+        self._latency += delta_latency
 
 
     @property
     def path(self):
-        return self.path
+        return self._path
 
 
     @path.setter
-    def path(self,path: list[str]):
-        self.path = path
+    def path(self,anypath):
+        if isinstance(anypath, list):  # check if value is a list to assign it to the path
+            self._path = anypath
 
 
-    def update_path(self):
-        if self.path:
-            self.path.pop(0)
+    def update_path(self,node):
+        self._path.remove(node)
 
 
 
 class Node(object):
-    def __init__(self):
-        pass
+    def __init__(self, node_info: dict, label:str):
+        self._label = label
+        self._position = node_info.get("position", (0, 0))  # tuple
+        self._connected_nodes = node_info.get("connected_nodes", [])
+        self._successive = {}  # empty dict
 
     @property
     def label(self):
-        pass
+        return self._label
+    @label.setter
+    def label(self, label):
+        self._label = label
 
     @property
     def position(self):
-        pass
+        return self._position
+    @position.setter
+    def position(self, position):
+        self._position = position
 
     @property
     def connected_nodes(self):
-        pass
+        return self._connected_nodes
+    @connected_nodes.setter
+    def connected_nodes(self, connected_nodes):
+        self._connected_nodes = connected_nodes
 
     @property
     def successive(self):
-        pass
+        return self._successive
 
     @successive.setter
-    def successive(self):
-        pass
+    def successive(self,successive):
+        self._successive = successive
 
-    def propagate(self):
-        pass
+    def propagate(self, sig: Signal_information):
+        for node in sig.path:
+            if sig.path:
+                if node == self._label:
+                    sig.update_path(node)  # remove the node crossed by the signal
+                    line = self.successive[node + sig.path[0]]
+                    line.propagate(sig)
 
 
 class Line(object):
